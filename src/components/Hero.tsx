eb3fc2cf4846c8c3e6c9fbfe1395269f7aeca1e0
Kevin Wiratama Sangehao Hia <kevin063@gmail.com>
2025-11-10 09:52:41 +0700

    Add TextType component to Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,27 @@
 // src/components/Hero.tsx
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import TextType from "./TextType";
 
 export default function Hero() {
   return (
     <section id="beranda" className="bg-gray-50">
       <div className="container mx-auto flex h-[calc(100vh-80px)] min-h-[500px] flex-col items-center justify-center px-4 text-center">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+        {/*<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
           Solusi Hukum Profesional
           <span className="block text-blue-700">
             untuk Bisnis Anda yang Kompleks
           </span>
+        </h1>*/}
+
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          <TextType
+            text={["EGUL ASSOCIATES LAW FIRM",""]}
+            typingSpeed={75}
+            pauseDuration={1500}
+            showCursor={true}
+            cursorCharacter="|"
+          />
         </h1>
 
         <p className="mt-6 max-w-2xl text-lg leading-8 text-gray-600">
